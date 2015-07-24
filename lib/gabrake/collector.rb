d9299825ebcbd863b4c060f1d7f6da5fce18dbf4
Samuel Molnar <molnar.samuel@gmail.com>
2015-07-24 17:55:04 +0200

    Add notifier for Google Analytics

---
+++ lib/gabrake/collector.rb
@@ -0,0 +1,52 @@
+module Gabrake
+  class Collector
+    URL = 'http://www.google-analytics.com/collect'
+
+    def self.event_for(exception, context)
+      event = Event.new(exception)
+
+      unless Gabrake.tracking_id
+        raise ArgumentError.new('You need to specify Google Analytics Tracking ID in `Gabrake.tracking_id\'')
+      end
+
+      params = {
+        v:   context[:version],
+        dl:  context[:url],
+        cid: context[:client_id],
+        tid: Gabrake.tracking_id,
+        t:   :event,
+        ec:  event.category,
+        ea:  event.action,
+        el:  event.label
+      }
+
+      params[:"cd#{Gabrake.custom_dimension_index}"] = Gabrake.tracked_version if Gabrake.custom_dimension_index
+
+      "#{URL}?#{params.map { |key, value| "#{key}=#{value}" }.join('&') }"
+    end
+
+    class Event
+      def initialize(exception)
+        @exception = exception
+      end
+
+      def category
+        'Gabrake (Rails)'
+      end
+
+      def action
+        @message ||= "#{@exception.class}: #{@exception.message}"
+      end
+
+      def label
+        "#{location.path.gsub(/\A#{::Rails.root}\//, '')}:#{location.lineno}" if location
+      end
+
+      private
+
+      def location
+        @location ||= @exception.backtrace_locations.try(:first)
+      end
+    end
+  end
+end
---
+++ lib/gabrake/notifier.rb
@@ -0,0 +1,23 @@
+module Gabrake
+  class Notifier
+    def self.deliver(exception, env:, **options)
+      context   = Env.extract_context(env)
+      event_url = Gabrake::Collector.event_for(exception, context)
+
+      HTTParty.get(URI.encode(event_url), env.slice('User-Agent'))
+    end
+
+    class Env
+      def self.extract_context(env)
+        cookies = env['action_dispatch.cookies']
+        _, version, client_id = *(cookies['_ga'] || 'GA1.1.1.1').match(/GA(\d+)\..+\.(\d+\.\d+)\z/)
+
+        {
+          version: version,
+          client_id: client_id,
+          url: env['REQUEST_URI']
+        }
+      end
+    end
+  end
+end
